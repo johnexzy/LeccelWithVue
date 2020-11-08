dbb7d8db6bebf98b30e7f754a8dc6e5b544660c0
Johnexzy <obajohn75@gmail.com>
2020-11-08 19:59:57 +0100

    refactor(created a model for music): music data model, was created

---
+++ src/model/musicobj.js
@@ -0,0 +1,16 @@
+const musicObj = {
+    id: "48",
+    music_name: "phyno - cho life",
+    music_details: "joj xos xpo noiox onio",
+    artist: "john",
+    music_key: "d91b59d031172b63f11915d2c73244a8",
+    short_url: "phyno_-_cho_life-1129312831",
+    popular: "0",
+    uploaded_by: "Leccel",
+    created_at: "2020-10-16 10:10:10",
+    updated_at: "2020-10-16 10:10:10",
+    images: ["uploads/images/20201016111010951062254.jpg"],
+    comments: [],
+};
+
+module.exports = musicObj
---
+++ src/model/relatedMusicobj.js
@@ -0,0 +1,24 @@
+const relatedMusicObj = [
+    {
+      id: "50",
+      music_name: "arx seor oinwe",
+      music_details: "joj xos xpo noiox onio",
+      artist: "john",
+      music_key: "3c04d7f70e950eb5ec7afb7b466f8e7c",
+      short_url: "arx_seor_oinwe-1110462004",
+      popular: "0",
+      uploaded_by: "Leccel",
+      created_at: "2020-10-16 10:12:20",
+      updated_at: "2020-10-16 10:12:20",
+      audio: [
+        {
+          song_url: "uploads/audios/arx_seor_oinwe-john73.mp3",
+          song_bytes: "5607640",
+        },
+      ],
+      images: ["uploads/images/20201016111220170513104.png"],
+      comments: [],
+    },
+  ]
+
+  module.exports = relatedMusicObj
