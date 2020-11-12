a5a62032847e87d9a773529e0b70af39b73f23e2
Johnexzy <obajohn75@gmail.com>
2020-11-12 23:44:13 +0100

    refactor(ffle): large assets size
    Deleted Unneccessary assets data

--- src/config/apiservice.js
+++
@@ -1,8 +0,0 @@
-// import api from 'axios';
-// import { Api_Base } from '@/config/config.js'
-const axios = require('axios');
-const { Api_Base } = require('../config/config.js');
-// export default{
-//   api: api.create({baseURL: Api_Base})
-// }
-module.exports = { api: axios.create({baseURL: Api_Base}) }
