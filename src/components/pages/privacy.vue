acec263f72cdc7d998c3f5dfe113a796ff337f7e
Johnexzy <obajohn75@gmail.com>
2020-11-19 17:58:16 +0100

    added privacy component

---
+++ src/components/pages/privacy.vue
@@ -0,0 +1,85 @@
+
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">Privacy Policy</div>
+      <div class="card-body">
+        <div class="aboutus-wrapper">
+          <img
+            src="/assets/images/privacy.jpg"
+            alt="banner"
+            class="img-fluid mb-5"
+          >
+          <div class="h2">
+            These terms apply when you use our online services on Leccel.net
+          </div>
+          <p class="fs-15">
+            Please read these terms carefully before using our online services
+            as you will be bound by these terms. If you do not agree to any of
+            these terms we do not grant you permission to use our online
+            services. If you have any questions you can contact us using our
+            email contact form. Who we Are Our website address is:
+            <b class="text-monospace">http://Leccel.net.</b>
+
+            Data Collection When visitors leave comments on the site we collect
+            the data shown in the comments section, and also the visitor’s IP
+            address and browser user agent string to help spam detection. An
+            anonymized string created from your email address (also called a
+            hash) may be provided to the Gravatar service to see if you are
+            using it. The Gravatar service privacy policy is available here:
+            https://automattic.com/privacy/. After approval of your comment,
+            your profile picture is visible to the public in the context of your
+            comment. Cookies and Web Beacons The Internet pages of the Leccel
+            Media use cookies. Cookies are text files that are stored in a
+            computer system via an Internet browser. Many Internet sites and
+            servers make use cookies. Many cookies contain a so-called cookie
+            ID. A cookie ID is a unique identifier of the cookie. It made up of
+            a character string through which Internet pages and servers can be
+            assigned to the specific Internet browser in which the cookie was
+            stored. This allows visited Internet sites and servers to
+            differentiate the individual browser of the dats subject from other
+            Internet browsers that contain other cookies. A specific Internet
+            browser can be recognized and identified using the unique cookie ID.
+            Through the use of cookies, Leccel.net can provide the users of this
+            website with more user-friendly services that would not be possible
+            without the cookie setting. By means of a cookie, the information
+            and offers on our website can be optimized with the user in mind.
+            Cookies allow us, as previously mentioned, to recognize our website
+            users. The purpose of this recognition is to make it easier for
+            users to utilize our website. The website user that uses cookies,
+            example, Does not have to enter access data each time the website is
+            accessed, because this is taken over by the website, and the cookie
+            is thus stored on the user's computer system. The data subject may,
+            at any time, prevent the setting of cookies through our website by
+            means of a corresponding setting of the Internet browser used, and
+            may thus permanently deny the setting of cookies. Furthermore,
+            already set cookies may be deleted at any time via an Internet
+            browser or other software programs. This is possible in all popular
+            Internet browsers. If the data subject deactivates the setting of
+            cookies in the Internet browser used, not all functions of our
+            website may be entirely usable. THIRD PARTY Leccel Media may contain
+            links to other websites which are not controlled by us. Any such
+            links are provided solely for your convenience and this does not
+            amount to an endorsement by us of that website or its content. If
+            you use those links you will leave our online services and if you
+            decide to visit any linked site, you do so at your own risk. These
+            terms only apply to your use of our online services and we cannot be
+            responsible for any content, or products and services available on
+            any other websites as we do not control them. TAKE DOWN If you want
+            to make a complaint about any of our online services or if you think
+            your or someone else’s intellectual property or other rights have
+            been infringed by our online services you can do so by submitting a
+            complaint to our email admin@leccel.net.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-irregular-whitespace */
+export default {
+  name: "Privacy",
+};
+</script>
