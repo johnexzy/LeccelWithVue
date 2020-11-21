70279de724cf34c2b3d666ba543e9f2eafbb0b19
Johnexzy <obajohn75@gmail.com>
2020-11-21 14:25:54 +0100

    Added PWA support

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,28 @@
-module.exports = {}
+const {GenerateSW} = require('workbox-webpack-plugin');
+module.exports = {
+    // ...other vue-cli plugin options...
+    configureWebpack: {
+        plugins: [
+          new GenerateSW()
+        ]
+      },
+    pwa: {
+      name: 'Leccel.net',
+      themeColor: '#4d7cff',
+      msTileColor: '#2b0d0d',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+  
+      // configure the workbox plugin
+      workboxPluginMode: 'GenerateSW',
+      iconPaths:{
+        favicon32: 'img/icons/favicon-32x32.png',
+        favicon16: 'img/icons/favicon-16x16.png',
+        appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+        maskIcon: 'img/icons/safari-pinned-tab.svg',
+        msTileImage: 'img/icons/msapplication-icon-144x144.png'
+      },
+      manifestPath:'manifest.json',
+      manifestOptions:{}
+    }
+  }
