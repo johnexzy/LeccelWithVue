9a5491be17a1f52a712163293637e6233704bea0
Johnexzy <obajohn75@gmail.com>
2020-12-04 11:58:27 +0100

    refactor(toggled): devMode
    Toggled Devmode

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const devMode = false;
+const devMode = true;
 const Api_Base = devMode ? 'http://127.0.0.1:8090' : 'https://app.leccel.net'
 module.exports = {
     Api_Base: Api_Base,
