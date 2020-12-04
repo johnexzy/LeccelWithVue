412427fd64e5e49c8940c4c464ef2527a60547b9
Johnexzy <obajohn75@gmail.com>
2020-12-04 12:00:27 +0100

    chore(package): added Vue_aplayr package

--- src/main.js
+++ src/main.js
@@ -4,14 +4,23 @@ import App from './App.vue';
 import router from './router'
 import store from './store/index';
 import NProgress from 'nprogress';
+import VueAPlayer from 'vue-aplayer'
 import comment from "@/components/utils/comments/comments.vue";
-
+import "@mdi/font/scss/materialdesignicons.scss";
+import 'nprogress/nprogress.css';
+import '../public/assets/css/style.css';
 import carouselImg from "@/components/utils/carousel/carouselImage.vue";
 import share from "@/components/utils/share/share.vue";
+import 'jquery'
+import 'bootstrap'
+import 'popper.js'
 import './registerServiceWorker'
+Vue.component('aplayer', VueAPlayer)
 Vue.use(VueMeta)
 Vue.config.productionTip = true;
-
+// import 'jquery';
+// import 'bootstrap'
+// import 'popper.js'
 router.beforeResolve((to, from, next) =>{
     if (to.name) {
         NProgress.start()
@@ -33,3 +42,5 @@ new Vue({
     router,
     render: h => h(App)
 }).$mount('#app');
+
+
